995ef166500e9229425df9bbe97ff80ce1bc8fb1
nima_ab <nimamd24@gmail.com>
2020-06-05 23:56:45 +0430

    add input functionality for the second language to check a given string

--- pda/l2.py
+++ pda/l2.py
@@ -12,3 +12,20 @@ dfa = DFA(
     init_state='q0',
     final_states={'q1'}
 )
+
+input_string = input('Enter the string you wanna check: ')
+print('===============================')
+
+if dfa.accept_input_str(input_string):
+    print(f'String \'{input_string}\' is accepted by this language and the transitions are:')
+    visited_states, _ = dfa.read_input_str(input_string)
+
+    print(' -> ', end='')
+    for i in range(len(visited_states)):
+        print(visited_states[i], end='')
+
+        if i != len(visited_states) - 1:
+            print(' => ', end='')
+
+else:
+    print(f'String \'{input_string}\' is not accepted by this language!')
