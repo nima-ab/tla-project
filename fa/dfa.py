9bc91b4d9cf77b7e8a370a74d5b7690bed48ff7f
nima_ab <nimamd24@gmail.com>
2020-06-05 23:51:18 +0430

    add get next state method for dfa

--- fa/dfa.py
+++ fa/dfa.py
@@ -5,3 +5,9 @@ class DFA:  # TODO add description to the readme.md file and comment the damn co
         self.transitions = transitions
         self.init_state = init_state
         self.final_states = final_states
+
+    def _get_next_state(self, current_state, input_symbol):
+        if input_symbol in self.transitions[current_state]:
+            return self.transitions[current_state][input_symbol]
+        else:
+            raise Exception('Invalid Symbol!')
